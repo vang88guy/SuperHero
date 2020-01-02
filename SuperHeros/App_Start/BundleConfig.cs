f3f94e2595d8db9dca6b358f5d4e0265942fd70a
Chong <chong_1988@hotmail.com>
2020-01-02 15:37:35 -0600

    Change bootstrap

--- SuperHeros/App_Start/BundleConfig.cs
+++ SuperHeros/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SuperHeros
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstraphero.css",
                       "~/Content/site.css"));
         }
     }

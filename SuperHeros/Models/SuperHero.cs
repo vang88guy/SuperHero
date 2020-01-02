46c984272fa61436f89be2399f24c6be0734f85a
Chong <chong_1988@hotmail.com>
2020-01-02 10:56:50 -0600

    Initial commit and made SuperHero class in model folder

---
+++ SuperHeros/Models/SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeros.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string alterEgo { get; set; }
+        public string primarySuperheroAbility { get; set; }
+        public string secondarySuperheroAbility { get; set; }
+        public string catchpharse { get; set; }
+    }
+}
